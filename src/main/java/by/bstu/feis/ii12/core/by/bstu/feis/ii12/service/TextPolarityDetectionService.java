bce353c592e2e9121b36dc620b176a4a4b06dfc5
Vadzim Parafianiuk <vadzim_parafianiuk@epam.com>
2017-12-09 12:42:57 +0300

    init

---
+++ src/main/java/by/bstu/feis/ii12/core/by/bstu/feis/ii12/service/TextPolarityDetectionService.java
@@ -0,0 +1,25 @@
+package by.bstu.feis.ii12.core.by.bstu.feis.ii12.service;
+
+import by.bstu.feis.ii12.core.TextPolarityDetector;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TextPolarityDetectionService {
+
+    List<TextPolarityDetector> detectors = new ArrayList<TextPolarityDetector>();
+
+    public boolean getTextPolarity(String text) {
+         Integer results =
+                detectors
+                    .stream()
+                    .mapToInt(detector -> {
+                        if (detector.isPositive(text)) return 1;
+                        else return 0;
+                    })
+                    .sum();
+
+        return results > detectors.size() / 2;
+    }
+}
